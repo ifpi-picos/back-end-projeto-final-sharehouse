2ee97160a8be202f79a88b708ff1f69af02d5069
Paulo Sobrinho Ferreira <paulojhole@gmail.com>
2020-01-23 14:35:51 -0300

    ajuste

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/no-dynamic-require */
-const Config = require(`./env/${(process.env.NODE_ENV || 'production')}.js`);
+const Config = require(`./env/${(process.env.NODE_ENV || 'development')}.js`);
 
 module.exports = Config;
--- src/config/env/development.js
+++ src/config/env/development.js
@@ -3,9 +3,12 @@ class Config {
     this.env = 'development';
     this.PORT = process.env.PORT || 3000;
     this.API_BASE = '/api';
-    const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
+    const DB_DB = process.env.DB_DB ? process.env.DB_DB : 'b915jnoavh0fjlh';
+    const DB_USER = process.env.DB_USER ? process.env.DB_USER : 'ugp6drvosrrqrfebs2ey';
+    const DB_PASSWORD = process.env.DB_PASSWORD ? process.env.DB_PASSWORD : 'o0FsqXM4oUoEiBcSBBSi';
+    const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'b915jnoavh0fjlh-mongodb.services.clever-cloud.com';
     const DB_PORT = process.env.DB_PORT ? process.env.DB_PORT : '27017';
-    this.MONGODB_URL = `mongodb://${DB_HOST}':'${DB_PORT}/db_dev`;
+    this.MONGODB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DB}`;
   }
 }
 
